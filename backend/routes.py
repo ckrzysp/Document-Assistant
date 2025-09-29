4cbaa851db1ff3d3001b035ff22b7bf24a3a2abc
Patrick Lin <patrick@2ndset.ai>
2025-09-29 15:15:17 -0400

    Initialize FastAPI backend with MariaDB connection and table setup.
    AI Usage:
    - Suggested file structure
    - Provided examples for initial FastAPI–MariaDB connection setup
    - Assisted with debugging
    - Generated Pydantic models for API endpoints

---
+++ backend/routes.py
@@ -0,0 +1,54 @@
+# Endpoint Code
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import database, engine
+from schemas import LoginRequest, LoginResponse, RegisterRequest, RegisterResponse
+from crud.user_crud import UserCRUD
+from passlib.context import CryptContext
+from crud.user_crud import UserCRUD
+
+app = FastAPI()
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+def get_db():
+    db = Session(bind=engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+@app.post("/register", response_model=RegisterResponse)
+async def register(request : RegisterRequest, db : Session = Depends(get_db)):
+    user = UserCRUD.get_by_email(db=db, email=request.email)
+
+    if user:
+        raise HTTPException(status_code=400, detail="Email is already used.")
+
+    hashed_password = pwd_context.hash(request.password)
+    await UserCRUD.create(db=db, name=request.name, email=request.email, hashed_password=hashed_password)
+    return {"message" : "User created successfully"}
+
+
+@app.post("/login", response_model=LoginResponse)
+def login(request: LoginRequest, db: Session = Depends(get_db)):
+
+    user = UserCRUD.get_by_email(db=db, email=request.email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    if pwd_context.verify(request.password, user.hashed_password):
+        return LoginResponse(
+            success=True,
+            message="Login successful",
+            user_id=user.id
+        )
+    else:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
---
+++ backend/schemas.py
@@ -0,0 +1,19 @@
+# Pydantic Models for each endpoint (Both Request & Response)
+from pydantic import BaseModel, EmailStr
+
+class RegisterRequest(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+class RegisterResponse(BaseModel):
+    message: str
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class LoginResponse(BaseModel):
+    success: bool
+    message: str
+    user_id: int | None = None
