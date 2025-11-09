eb024b00c7298886786f383684615fb46000955a
pawluschristopher@gmail.com <pawluschristopher@gmail.com>
2025-11-09 17:22:53 -0500

    improved box count

--- neural_network/TestCNN.py
+++ neural_network/TestCNN.py
@@ -39,8 +39,10 @@ class ConvolutionalNN(NN.Module):
                NN.MaxPool2d(2,2)              
           )
 
-          self.box_head = NN.Conv2d(150, 4, 1)
-          self.class_head = NN.Conv2d(150, 4, 1)
+          num_boxes = 1
+          num_classes = 4
+          self.box_head = NN.Conv2d(150, num_boxes*4, 1)
+          self.class_head = NN.Conv2d(150, num_boxes*num_classes, 1)
 
      # Output Tensors, function used for training
      def forward(self, x):
@@ -48,8 +50,15 @@ class ConvolutionalNN(NN.Module):
           boxP = self.box_head(seq)
           classP = self.class_head(seq)
 
-          boxP = boxP.mean(dim=[2,3])
-          classP = classP.mean(dim=[2,3])
+          # Single Box
+          #boxP = boxP.mean(dim=[2,3])
+          #classP = classP.mean(dim=[2,3])
+
+          inSize = x.size(0)
+          boxP = boxP.permute(0,1,2,3).contiguous().view(1,-1,4)
+          #boxP = boxP.view(inSize,-1,4)
+          classP = classP.permute(0,1,2,3).contiguous().view(1,-1,4)
+          #classP = classP.view(inSize,-1,4)
 
           return boxP, classP
 
@@ -81,9 +90,71 @@ model = ConvolutionalNN()
 model.load_state_dict(torch.load(statepath, weights_only=True))
 model.to('cuda')
 
+boxcount = 5
+detected = 0
 # Show prediction on every image
 dirc = os.listdir(datapath_testing_image)
-for imaget in range(10):
+for imaget in range(boxcount):
+     # Take image, convert to tensor
+     img = os.listdir(datapath_testing_image)[imaget]
+     name = img
+     img = Image.open(datapath_testing_image +"\\"+ img).convert('RGB')
+     img_tensor = ToTensor()(img).unsqueeze(0).to('cuda')
+     img_tensor = torch.as_tensor(img_tensor, dtype=torch.float32, device='cuda')
+
+     # Converting it from tensor to readable numbers
+     fig, ax = pyp.subplots()
+     boxvector = []
+     tempx = 0
+     tempy = 0
+     box = model(img_tensor)[0]
+     clss = model(img_tensor)[1]
+     print(box)
+     print(clss)
+
+     for i in range(int(len(box[0][0]))):
+          # Dimensions
+          img_w, img_h = img.size
+          x, y = int(box[0][i][0].item()*img_w), int(box[0][i][1].item()*img_h)
+          box_w, box_h = int(box[0][i][2].item()*img_w), int(box[0][i][3].item()*img_h)
+
+          x = abs(x)
+          y = abs(y)
+          box_w = abs(box_w)
+          box_h = abs(box_h)
+
+          xp = x
+          yp = y
+          box_wp = box_w
+          box_hp = box_h
+          x1, y1 = xp - box_wp/2, yp - box_hp/2
+          x2, y2 = xp + box_wp/2, yp + box_hp/2
+
+          ax.imshow(img)
+          rect = patches.Rectangle((x1, y1), x2, y2, linewidth=2, edgecolor='r', facecolor='none')
+          tempx = x
+          tempy = y
+
+          boxvector.append(rect)
+     
+     for i in range(int(len(box[0][0]))):
+          ax.add_patch(boxvector[i])
+          ax.text(tempx, tempy, finder[torch.argmax(model(img_tensor)[0][0][i]).item()], color='red', fontsize=2)
+     
+     pyp.show()
+     pyp.close()
+     
+     # OUTPUT 
+     print("IMG: "+name+" --- Class: " + finder[torch.argmax(model(img_tensor)[0][0][i]).item()])
+
+     # print(torch.argmax(model(img_tensor)[0]).item())
+     print(len(boxvector))
+
+
+
+"""
+dirc = os.listdir(datapath_testing_image)
+for imaget in range(3):
      # Take image, convert to tensor
      img = os.listdir(datapath_testing_image)[imaget]
      name = img
@@ -93,30 +164,40 @@ for imaget in range(10):
 
      # Add Confidence
      # Converting it from tensor to readable numbers
-     boxO = (model(img_tensor)[0])
-     print("\n")
-     print(model(img_tensor))
- 
-     # Dimensions
-     img_w, img_h = img.size
-     x, y = model(img_tensor)[0][0][0].item(), model(img_tensor)[0][0][1].item()
-     box_w, box_h = model(img_tensor)[0][0][2].item(), model(img_tensor)[0][0][3].item()
+     fig, ax = pyp.subplots()
+     boxvector = []
+     tempx = 0
+     tempy = 0
+     for i in range(boxcount):
+          boxO = (model(img_tensor)[0])
+          print(boxO)
+     
+          # Dimensions
+          print("\n")
+          img_w, img_h = img.size
+          x, y = int(model(img_tensor)[0][0][i][0].item()*img_w), int(model(img_tensor)[0][0][i][1].item()*img_h)
+          box_w, box_h = int(model(img_tensor)[0][0][i][2].item()*img_w), int(model(img_tensor)[0][0][i][3].item()*img_h)
 
-     xp = x*img_w
-     yp = y*img_h
-     box_wp = box_w*img_w
-     box_hp = box_h*img_h
-     x1, y1 = xp - box_wp/2, yp - box_hp/2
+          xp = x*img_w
+          yp = y*img_h
+          box_wp = box_w*img_w
+          box_hp = box_h*img_h 
+          x1, y1 = xp - box_wp/2, yp - box_hp/2
 
-     fig, ax = pyp.subplots(1)
-     ax.imshow(img)
-     rect = patches.Rectangle((x1, y1), box_wp, box_hp, linewidth=2, edgecolor='r', facecolor='none')
-     ax.add_patch(rect)
-     ax.text(x, y-5, finder[torch.argmax(model(img_tensor)[0]).item()], color='red', fontsize=10, backgroundcolor='white')
+          #fig, ax = pyp.subplots()
+          ax.imshow(img)
+          rect = patches.Rectangle((x, y), box_w, box_h, linewidth=2, edgecolor='r', facecolor='none')
+          tempx = x
+          tempy = y
 
+          boxvector.append(rect)
+
+     for i in range(boxcount):
+          ax.add_patch(boxvector[i])
+          ax.text(tempx, tempy, finder[torch.argmax(model(img_tensor)[0][0][i]).item()], color='red', fontsize=2)
      pyp.show()
      pyp.close()
-
      # OUTPUT 
-     print("IMG: "+name+" --- Class: " + finder[torch.argmax(model(img_tensor)[0]).item()])
+     print("IMG: "+name+" --- Class: " + finder[torch.argmax(model(img_tensor)[0][0][i]).item()])
      # print(torch.argmax(model(img_tensor)[0]).item())
+"""
